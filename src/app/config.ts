6b65ba3888aed8a71da476a3c98078fc40f3c6c8
epsjluiz <ledujunior@gmail.com>
2025-12-26 09:43:28 -0300

    DB HOST 3

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,2 @@
-export const API_BASE_URL =
-  (window as any).__API_URL__ || 'http://localhost:3000';
-
+const RAW_API_URL = (window as any).__API_URL__ || 'http://localhost:3000';
+export const API_BASE_URL = RAW_API_URL.replace(/\/+$/, '');
